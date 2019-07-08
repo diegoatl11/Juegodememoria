471a33fac503e68136b1fbde290572232f1600aa
USER <USER@192.168.1.6>
2019-07-08 15:10:50 -0500

    repositorio juego memoria actulizacion (1.1)

---
+++ src/IU/App_Juego.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IU;
+
+/**
+ *
+ * @author USER
+ */
+public class App_Juego extends javax.swing.JFrame {
+
+    /**
+     * Creates new form App_Juego
+     */
+    public App_Juego() {
+        initComponents();
+        this.setLocationRelativeTo(null);
+    }
+
+ 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jLabel1 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        botonInicio = new javax.swing.JMenuItem();
+        botonSalir = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        botonivel1 = new javax.swing.JMenuItem();
+        botonnivel2 = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/IU/Logo.PNG"))); // NOI18N
+
+        jMenu1.setText("File");
+
+        botonInicio.setText("INICIAR");
+        botonInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonInicioActionPerformed(evt);
+            }
+        });
+        jMenu1.add(botonInicio);
+
+        botonSalir.setText("SALIR");
+        botonSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonSalirActionPerformed(evt);
+            }
+        });
+        jMenu1.add(botonSalir);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Niveles");
+
+        botonivel1.setText("NIVEL 1");
+        botonivel1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonivel1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(botonivel1);
+
+        botonnivel2.setText("NIVEL 2");
+        botonnivel2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonnivel2ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(botonnivel2);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void botonnivel2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonnivel2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botonnivel2ActionPerformed
+
+    private void botonSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonSalirActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_botonSalirActionPerformed
+
+    private void botonInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonInicioActionPerformed
+        // TODO add your handling code here:
+        AppPuntoIU  vista = new AppPuntoIU();
+        //ver el nivel 1
+        vista.setVisible(true);
+        //ver el nivel 2
+        this.setVisible(false);
+    }//GEN-LAST:event_botonInicioActionPerformed
+
+    private void botonivel1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonivel1ActionPerformed
+        // TODO add your handling code here:
+         AppPuntoIU  vista1 = new AppPuntoIU();
+        //ver el nivel 1
+        vista1.setVisible(true);
+        //ver el nivel 2
+        this.setVisible(false);
+    }//GEN-LAST:event_botonivel1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(App_Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(App_Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(App_Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(App_Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new App_Juego().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem botonInicio;
+    private javax.swing.JMenuItem botonSalir;
+    private javax.swing.JMenuItem botonivel1;
+    private javax.swing.JMenuItem botonnivel2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    // End of variables declaration//GEN-END:variables
+}
