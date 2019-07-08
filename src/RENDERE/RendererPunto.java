8c9142ad12fa7f9d0c6e22ace8f064d3f26ad8cf
USER <USER@192.168.1.6>
2019-07-08 14:18:31 -0500

    repositorio juego memoria actulizacion (1.0)

--- src/RENDERE/RendererPunto.java
+++ src/RENDERE/RendererPunto.java
@@ -12,35 +12,40 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.util.gl2.GLUT;
 import java.awt.Component;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class RendererPunto implements GLEventListener ,MouseListener, MouseMotionListener {
+public class RendererPunto implements GLEventListener, MouseListener, MouseMotionListener {
 
     private Punto punto;
     private GL2 gl;
     private GLU glu;
     private GLUT glut;
+    double rot0, rot1, rot2, rot3,rot4,rot5;
     double theta;
-    
     double Rotacion;
- 
+
     double Sx;
     double Sy;
     double Sz;
 
-    
     public RendererPunto() {
         punto = new Punto();
         glu = new GLU();
         glut = new GLUT();
-       
+
         Rotacion = 0;
         Sx = 1;
         Sy = 1;
         Sz = 1;
-        theta=0;
+        rot0 = 0;
+        rot1 = 0;
+        rot2 = 0;
+        rot3 = 0;
+        rot4 = 0;
+        rot5 = 0;
 
     }
     //nuevo Metodo
@@ -72,8 +77,8 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
         gl.glMatrixMode(GL2.GL_MODELVIEW);
 
         glu.gluLookAt(10, 0, 50,
-                      0, 0, 0, 
-                      0, 1, 0);
+                0, 0, 0,
+                0, 1, 0);
 
         gl.glDrawBuffer(GL2.GL_FRONT_AND_BACK);
 
@@ -109,16 +114,16 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
         gl.glPushMatrix();
         gl.glColor3d(1, 1, 0);
         gl.glTranslatef(0, 0, 0);
-        gl.glRotated(theta, 0,1,0);
+        gl.glRotated(rot0, 0, 1, 0);
         glut.glutSolidCube(10);
         gl.glPopMatrix();
     }
 
     public void Cubo1() {
         gl.glPushMatrix();
-        gl.glColor3d(0, 1, 0);
+        gl.glColor3d(0, 1, 1);
         gl.glTranslatef(-15, 0, 0);
-        gl.glRotated(theta, 0,1,0);
+        gl.glRotated(rot1, 0, 1, 0);
         glut.glutSolidCube(10);
         gl.glPopMatrix();
     }
@@ -128,7 +133,7 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
         gl.glPushMatrix();
         gl.glColor3d(0, 1, 0);
         gl.glTranslatef(15, 0, 0);
-        gl.glRotated(theta, 0,1,0);
+        gl.glRotated(rot2, 0, 1, 0);
         glut.glutSolidCube(10);
         gl.glPopMatrix();
 
@@ -137,9 +142,9 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
     public void Cubo3() {
         //Cubo
         gl.glPushMatrix();
-        gl.glColor3d(0, 1, 0);
+        gl.glColor3d(1, 0, 0);
         gl.glTranslatef(-15, -15, 0);
-        gl.glRotated(theta, 0,1,0);
+        gl.glRotated(rot3, 0, 1, 0);
         glut.glutSolidCube(10);
         gl.glPopMatrix();
 
@@ -148,9 +153,9 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
     public void Cubo4() {
         //Cubo
         gl.glPushMatrix();
-        gl.glColor3d(0, 1, 0);
+        gl.glColor3d(0, 0, 1);
         gl.glTranslatef(0, -15, 0);
-        gl.glRotated(theta, 0,1,0);
+        gl.glRotated(rot4, 0, 1, 0);
         glut.glutSolidCube(10);
         gl.glPopMatrix();
 
@@ -159,10 +164,10 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
     public void Cubo5() {
         //Cubo
         gl.glPushMatrix();
-        gl.glColor3d(0, 1, 0);
+        gl.glColor3d(1, 1, 1);
         gl.glTranslatef(15, -15, 0);
-        gl.glRotated(theta, 0,1,0);
-        
+        gl.glRotated(rot5, 0, 1, 0);
+
         glut.glutSolidCube(10);
         gl.glPopMatrix();
 
@@ -175,33 +180,94 @@ public class RendererPunto implements GLEventListener ,MouseListener, MouseMotio
 
     @Override
     public void mouseClicked(MouseEvent e) {
-          
+
         int tempX = e.getX();
         int tempY = e.getY();
-        System.out.println(" RatonX: "+ tempX+" - RatonY :"+tempY);
+
+        System.out.println(" RatonX: " + tempX + " - RatonY :" + tempY);
 
         Component c = e.getComponent();
         double width = c.getWidth() / 2;
         double height = c.getHeight() / 2;
         
-        if((tempX >= 190) && (tempX <= 230)){
-            System.out.println("funcion");
-            theta = 80;
-        }else if ((tempX >= 231) && (tempX <= 290)){
-            System.err.println("regresa");
-            theta=0;
-            
+        // cubo0
+        if (tempX >= 190 && tempX <= 230) {
+            if (tempY >= 140 && tempY <= 200) {
+                System.out.println("funcion");
+                rot0 = 80;
+            }
+        } else if (tempX >= 231 && tempX <= 280) {
+            if(tempY >= 140 && tempY <= 200){
+                System.out.println("regresa");
+            rot0 = 0;
+            }
+        }
+        //cubo1
+        if (tempX >= 70 && tempX <= 109) {
+            if (tempY >= 140 && tempY <= 200) {
+                System.out.println("funcion");
+                rot1 = 80;
+            }
+        } else if (tempX >= 110 && tempX <= 150) {
+            if(tempY >= 140 && tempY <= 200){
+                System.out.println("regresa");
+            rot1 = 0;
+            }
+        }
+        //cubo2
+        if (tempX >= 320 && tempX <= 369) {
+            if (tempY >= 140 && tempY <= 200) {
+                System.out.println("funcion");
+                rot2 = 80;
+            }
+        } else if (tempX >= 370 && tempX <= 420) {
+            if(tempY >= 140 && tempY <= 200){
+                System.out.println("regresa");
+            rot2 = 0;
+            }
+        }
+        //cubo3
+        if (tempX >= 70 && tempX <= 109) {
+            if (tempY >= 230 && tempY <= 300) {
+                System.out.println("funcion");
+                rot3 = 80;
+            }
+        } else if (tempX >= 110 && tempX <= 150) {
+            if(tempY >= 230 && tempY <= 300){
+                System.out.println("regresa");
+            rot3 = 0;
+            }
+        }
+        //cubo4 
+        if (tempX >= 190 && tempX <= 229) {
+            if (tempY >= 230 && tempY <= 300) {
+                System.out.println("funcion");
+                rot4 = 80;
+            }
+        } else if (tempX >= 230 && tempX <= 280) {
+            if(tempY >= 230 && tempY <= 300){
+                System.out.println("regresa");
+            rot4 = 0;
+            }
+        }
+        //cubo5
+        if (tempX >= 320 && tempX <= 369) {
+            if (tempY >= 230 && tempY <= 300) {
+                System.out.println("funcion");
+                rot5 = 80;
+            }
+        } else if (tempX >= 370 && tempX <= 420) {
+            if(tempY >= 230 && tempY <= 300){
+                System.out.println("regresa");
+            rot5 = 0;
+            }
         }
 
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-     
-      
-        
-       
-        
+
         IU.AppPuntoIU.glCanvas.repaint();
     }
 
